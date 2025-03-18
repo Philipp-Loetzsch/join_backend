11a58ad89a0837e59a1fcbbd38f1e8003503c4eb
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-03-18 17:55:19 +0100

    remove unused code

--- join_app/api/serializer.py
+++ join_app/api/serializer.py
@@ -1,18 +1,11 @@
 from rest_framework import serializers
 from join_app.models import Task, Subtask, UserContact
-# Contact, 
 
 
 class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
         fields = '__all__'
-
-# class ContactSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Contact
-#         fields = '__all__'
-#         read_only_fields = ['user']
 
 class UserContactSerializer(serializers.ModelSerializer):
     class Meta:
@@ -46,15 +39,3 @@ class SubtaskSerializer(serializers.ModelSerializer):
         model = Subtask
         fields = '__all__'
         read_only_fields = ['task']
-# class TaskSerializer(serializers.ModelSerializer):
-#     subtasks = SubtaskSerializer(many=True, read_only=True)
-#     subtask_ids = serializers.PrimaryKeyRelatedField(
-#         queryset=Subtask.objects.all(),
-#         many=True,
-#         write_only=True,
-#         source="subtasks"
-#     )
-
-#     class Meta:
-#         model = Task
-#         fields = '__all__'
--- join_app/api/views.py
+++ join_app/api/views.py
@@ -1,51 +1,43 @@
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from join_app.models import Task, UserContact, Subtask
 from .serializer import TaskSerializer, UserContactSerializer, SubtaskSerializer
-from join_app.permissions import IsOwner
+
 
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()  # Kein extra Filter nötig
 
-
 class UserTaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
 
     def perform_create(self, serializer):
-        """Setzt den User automatisch beim Erstellen"""
         serializer.save(user=self.request.user)
-
 
 class UserContactListCreateView(generics.ListCreateAPIView):
     serializer_class = UserContactSerializer
 
+    def get_queryset(self):
+        return UserContact.objects.filter(user=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
 
 class UserContactDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserContactSerializer
     queryset = UserContact.objects.all()
 
-
 class SubtaskListCreateView(generics.ListCreateAPIView):
     serializer_class = SubtaskSerializer
 
     def get_queryset(self):
-        """Nur Subtasks von Tasks des aktuellen Users abrufen"""
         return Subtask.objects.filter(task__user=self.request.user)
 
     def perform_create(self, serializer):
-        """Stellt sicher, dass der Subtask nur zu eigenen Tasks hinzugefügt wird"""
         task_id = self.kwargs["task_id"]
         task = get_object_or_404(Task, id=task_id, user=self.request.user)
         serializer.save(task=task)
-
 
 class SubtaskDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SubtaskSerializer
