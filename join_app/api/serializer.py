2d9dba10a40696893be67ee229efd80a2216e8bf
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-04-02 15:10:33 +0200

    remove unused code again

--- join_app/api/serializer.py
+++ join_app/api/serializer.py
@@ -11,40 +11,6 @@ class UserContactSerializer(serializers.ModelSerializer):
         model = UserContact
         exclude = ['user']
 
-# class TaskSerializer(serializers.ModelSerializer):
-
-#     subtasks = SubtaskSerializer(many=True, read_only=True)
-  
-#     subtasks_data = SubtaskSerializer(many=True, write_only=True, required=False, source='subtasks')
-    
-#     assignTo = UserContactSerializer(many=True, read_only=True)
-#     assignTo_ids = serializers.PrimaryKeyRelatedField(
-#         queryset=UserContact.objects.all(),
-#         many=True,
-#         write_only=True,
-#         source='assignTo'
-#     )
-
-#     class Meta:
-#         model = Task
-#         fields = '__all__'
-#         read_only_fields = ['user']
-
-#     def create(self, validated_data):
-#         subtasks_data = validated_data.pop('subtasks', [])
-#         assignTo_ids = validated_data.pop('assignTo', [])
-              
-#         task = Task.objects.create(**validated_data)
-        
-#         for subtask_data in subtasks_data:
-#             Subtask.objects.create(task=task, **subtask_data)
-            
-#         task.assignTo.set(assignTo_ids)
-     
-#         task.refresh_from_db()
-#         return task
-
-
 class TaskSerializer(serializers.ModelSerializer):
     subtasks = SubtaskSerializer(many=True)
 
