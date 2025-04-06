256e51a33fc630a2d80c1abbaf7e552e6fd26f3a
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-04-06 21:23:28 +0200

    generate a readme

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)u32^m$_w9=7qjkgi00l2al(g*pogsytgdf6mlc7uc559#09(%"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
